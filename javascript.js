8c2a8591707518a0981ef5b7d2ccc7bb29e37bc7
Almazid11 <Almazid11@ymail.com>
2020-03-04 09:18:56 +0600

    First JS Update

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function parsing(str) {
+    var a = document.getElementById(str);
+    var b = parseFloat(a);
+    return b;
+}
+function Adding(num1,num2) {
+    var p = parseFloat(num1);
+    var q = parseFloat(num2);
+    var r = p + q;
+    return r;
+}
+function minus(num1,num2) {
+    var p = parseFloat(num1);
+    var q = parseFloat(num2);
+    var r = p - q;
+    return r;
+}
